a0ae0b966a422718b2de8a23600956d6350ba8b6
MaybeBorn2Code <64224843+MaybeBorn2Code@users.noreply.github.com>
2023-05-05 20:29:23 +0600

    updated(list of messages and retrieve) in views.py

--- messenger/apps/messeges/views.py
+++ messenger/apps/messeges/views.py
@@ -1,63 +1,53 @@
-# Django
-from django.shortcuts import render
 from django.db.models.query import QuerySet
+from django.shortcuts import get_object_or_404
 
-# DRF
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from rest_framework.decorators import action
+from django.dispatch import receiver
 
-# Local
-from messeges.models import (
-    Chat,
-    Message
+from mesages.mixins import (
+    ResponseMixin,
+    ObjectMixin,
 )
-from messeges.serializers import MessageSerializers, ChatSerializer
+from mesages.models import (
+    Message,
+    Chat,
+    User
+)
+from mesages.serializers import (
+    MessageSerializer,
+    ChatSerializer,
+    UserSerializer
+)
 
 
-class ChatMessageViewSet(ViewSet):
-    """
-    ViewSet Chats
-    """
+class MessageViewSet(ResponseMixin, ObjectMixin, ViewSet):
+    """ViewSet about chats and messages."""
 
-    queryset: QuerySet[Message] = Message.objects.select_related(
-        'to_send'
-    ).all()
+    queryset: QuerySet[Message] = \
+        Message.objects.select_related('to_send').all()
 
-    @action(
-        methods=['GET'],
-        detail=False
-    )
-    def list_chats(self, request: Request, *args: tuple) -> Response:
-        chats = [message.to_send for message in self.queryset]
-        serializer = ChatSerializer(
-            chats, many=True
+    # list of all
+    def list(self, request: Request, *args: tuple) -> Response:
+        """GET method."""
+
+        serializer: MessageSerializer = MessageSerializer(
+            self.queryset, many=True
         )
+
         return Response(
-            serializer.data,
-            status=200
+            data=serializer.data
         )
 
-    @action(
-        methods=['GET', 'POST'],
-        detail=False
-    )
-    def list_messages(self, request: Request, *args: tuple) -> Response:
-        if request.method == "GET":
-            serializer = MessageSerializers(
-                self.queryset, many=True
-            )
-            return Response(
-                serializer.data,
-                status=200
-            )
-        serializer: MessageSerializers = MessageSerializers(
-            data=request.POST
-        )
-        serializer.is_valid(raise_exception=True)
+    # retrieve only one message
+    def retrieve(self, request: Request, pk: int, *args: tuple) -> Response:
+        """GET method to retrieve a single message by ID."""
+
+        message: Message = get_object_or_404(self.queryset, pk=pk)
+        serializer: MessageSerializer = MessageSerializer(message)
+
         return Response(
-            {
-                'message': f"Message {serializer.validated_data.get('id')} is created"
-            }
+            data=serializer.data
         )
