4b457928b28c039d0800bab7e716969291b5e43a
alan <alansultanov00@gmail.com>
2023-05-12 21:11:07 +0600

    UK-2: Return active user model

--- messenger/apps/auths/utils.py
+++ messenger/apps/auths/utils.py
@@ -1,7 +1,4 @@
 from django.contrib.auth import get_user_model
 
 
-class Get_user_model:
-    def get_active_user_model():
-        User = get_user_model()
-        return User
+User = get_user_model()
