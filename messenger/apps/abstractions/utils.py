9998a31f01fe133f1b7662060aaddf513a616d0e
Богдан Зинченко <@itstep.krg>
2023-05-12 20:49:03 +0600

    UK-3. moved API_TOKEN to settings\base.py

--- messenger/apps/abstractions/utils.py
+++ messenger/apps/abstractions/utils.py
@@ -1,22 +1,22 @@
 import requests
 import datetime
-import decouple
+
+from settings.base import API_TOKEN
 
 
-def get_current_time_by_city_name(city_name: str) -> datetime.datetime:
+def get_current_time_or_none_by_city_name(city_name: str) -> datetime.datetime:
     URL: str = ('https://api.api-ninjas.com/v1/worldtime?'
                 f'city={city_name}')
-    API_TOKEN: str = decouple.config('CITY_TIME_API_TOKEN', cast=str)
     headers: dict[str, str] = {'X-Api-Key': API_TOKEN}
     r: requests.Response = requests.get(url=URL, headers=headers)
     # Check if response status code is 200
     status: int = r.status_code
     if status != 200:
-        return {'error': status}
+        return None
     # Get data from response and check if there any errors
     data: dict = r.json()
     if data.get('error'):
-        return data
+        return None
     # Create datetime variable to return
     time: datetime.datetime = datetime.datetime(
         year=int(data.get('year')),
