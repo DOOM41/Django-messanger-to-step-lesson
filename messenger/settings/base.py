27b3b86dd572535261976a2d98d65de13eb35f26
pra6801@gmail.com <johndoe@email.com>
2023-05-12 20:33:03 +0600

    uk-4. Start creating add Sentry

--- messenger/settings/base.py
+++ messenger/settings/base.py
@@ -113,19 +113,19 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# # Sentry
-# sentry_sdk.init(
-#     dsn="https://cebc4b414ade4f69a4cc6ee94788ddac@o4505120608944128.ingest.sentry.io/4505120612220928",
-#     integrations=[
-#         DjangoIntegration(),
-#     ],
+# Sentry
+sentry_sdk.init(
+    dsn="https://cebc4b414ade4f69a4cc6ee94788ddac@o4505120608944128.ingest.sentry.io/4505120612220928",
+    integrations=[
+        DjangoIntegration(),
+    ],
 
-#     # Set traces_sample_rate to 1.0 to capture 100%
-#     # of transactions for performance monitoring.
-#     # We recommend adjusting this value in production.
-#     traces_sample_rate=1.0,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
 
-#     # If you wish to associate users to errors (assuming you are using
-#     # django.contrib.auth) you may enable sending PII data.
-#     send_default_pii=True
-# )
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
