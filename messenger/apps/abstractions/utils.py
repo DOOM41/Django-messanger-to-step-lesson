cd729d0b79df2d382595e509807be055dfab5e23
Богдан Зинченко <@itstep.krg>
2023-05-12 20:12:09 +0600

    Create method to get city time by its name

---
+++ messenger/apps/abstractions/utils.py
@@ -0,0 +1,20 @@
+import requests
+import datetime
+
+
+def get_current_time_by_city_name(city_name: str) -> type:
+    URL: str = 'https://api.api-ninjas.com/v1/worldtime?city={}'.format(
+        city_name)
+    headers = {'X-Api-Key': 'f7xVZFQ//LSeJ1ym1TGgSw==hlohz1xQ8LzSqXPc'}
+    r: requests.Response = requests.get(url=URL, headers=headers)
+    data = r.json()
+    if data.get('error'):
+        return
+    time = datetime.datetime(
+        year=int(data.get('year')),
+        month=int(data.get('month')),
+        day=int(data.get('day')),
+        hour=int(data.get('hour')),
+        minute=int(data.get('minute')),
+        second=int(data.get('second')))
+    return time
